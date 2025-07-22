dafb618ed9832bbf4991165a201996b33db6eef9
Jonathan-dev2002 <jonathandoillon2002@gmail.com>
2025-07-22 15:51:37 +0700

    update project

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const authRoutes = require('./auth.routes');
 const userRoutes = require('./user.routes');
-const postRoutes = require('./post.routes');
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/', userRoutes); // User routes will be at /api/users, /api/admin/users etc.
-router.use('/posts', postRoutes);
+
 
 module.exports = router;
