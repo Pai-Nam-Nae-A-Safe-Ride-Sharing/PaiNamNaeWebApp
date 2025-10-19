32979add5068cfb2ae92a30183cee669e4bd5276
Jonathan-dev2002 <jonathandoillon2002@gmail.com>
2025-10-20 00:40:47 +0700

    fix refresh problem to production (deploy)

--- frontend/middleware/auth.js
+++ frontend/middleware/auth.js
@@ -1,4 +1,6 @@
 export default defineNuxtRouteMiddleware((to) => {
+  if (process.server) return
+
   const token = useCookie('token').value
   if (!token && to.path !== '/login') {
     return navigateTo('/login')
