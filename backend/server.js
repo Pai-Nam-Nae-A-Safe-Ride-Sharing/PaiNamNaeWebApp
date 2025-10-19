ab3421c836cd16b65ac26592c06f6011ceae0b8f
Jonathan-dev2002 <jonathandoillon2002@gmail.com>
2025-10-19 08:30:25 +0700

    update to deploy

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,6 @@ const corsOptions = {
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    optionsSuccessStatus: 200
 };
 
 app.use(cors(corsOptions));
